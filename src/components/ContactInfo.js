5ca1171f3cb5de23b452be7d2b751115634999df
blanketmanatee <ashleyl.welch@gmail.com>
2022-04-25 22:26:05 -0500

    back to working Contact page

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,12 +1,12 @@
 import React, {useContext, useState} from 'react'
 import Main from './Main'
 
-function AboutInfo(){
+function ContactInfo(){
     return (
         <div className="Main">
         <div className='About'>
                     <p>Contact</p>
-                    <p>Connect with us on social media. Learn more about Oklahoma Rare, our founders, and events we are attending.</p>
+                    <p>Connect with us on social media. Learn more about Oklahoma Rare, our founders, and events we are attending. I don't like the way this looks right now, I need to add more here. But this is the test to make sure it is still rendering when the footer is added. So lots of text lots of text lots of text. </p>
 
                 <p>Donate</p>
                 <p>Paypal button here?</p>
@@ -15,4 +15,4 @@ function AboutInfo(){
     )
 }
 
-export default AboutInfo
+export default ContactInfo
