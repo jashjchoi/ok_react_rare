352ae03c24f1c0b32c520acb74f676c9cfddcbe8
jashjchoi <jshjchoi@gmail.com>
2022-04-26 04:37:52 -0500

    login function

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,28 +1,28 @@
 import React from 'react';
 // import './assets/styles.css'
-import firebase from '../../firebase/index';
+// import firebase from '../../firebase/index';
 
-const test = async () => {
-    try {
-        var provider = new firebase.auth.GoogleAuthProvider()
-        const res = await firebase.auth().signInWithPopup(provider);
-        const user = res.user;
-        alert(user.displayName)
-        // const q = query(collection(db, "users"), where("uid", "==", user.uid));
-        // const docs = await getDocs(q);
-        // if (docs.docs.length === 0) {
-        //     await addDoc(collection(db, "users"), {
-        //         uid: user.uid,
-        //         name: user.displayName,
-        //         authProvider: "google",
-        //         email: user.email,
-        //     });
-        // }
-    } catch (err) {
-        console.error(err);
-        alert(err.message);
-    }
-}
+// const test = async () => {
+//     try {
+//         var provider = new firebase.auth.GoogleAuthProvider()
+//         const res = await firebase.auth().signInWithPopup(provider);
+//         const user = res.user;
+//         alert(user.displayName)
+//         // const q = query(collection(db, "users"), where("uid", "==", user.uid));
+//         // const docs = await getDocs(q);
+//         // if (docs.docs.length === 0) {
+//         //     await addDoc(collection(db, "users"), {
+//         //         uid: user.uid,
+//         //         name: user.displayName,
+//         //         authProvider: "google",
+//         //         email: user.email,
+//         //     });
+//         // }
+//     } catch (err) {
+//         console.error(err);
+//         alert(err.message);
+//     }
+// }
 
 
 const Login = () => {
@@ -31,7 +31,9 @@ const Login = () => {
         	<div className="inner">
 					<h3>Welcome to Oklahoma Rare</h3>
 					<center>
-						<button className="btn btn-dark btn-lg btn-block" onClick={ test}
+						{/* <button className="btn btn-dark btn-lg btn-block" onClick={ test}
+					>Sign In with Google</button> */}
+                    		<button className="btn btn-dark btn-lg btn-block"
 					>Sign In with Google</button>
 					</center>
 			</div>
