b3cdc8aaf0fcbed9f761037d2411d85415e5f807
jashjchoi <jashjchoi@gmail.com>
2022-04-27 16:07:36 -0500

    modified pages

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { auth} from '../firebase/index';
+import { auth } from '../firebase/index';
+import sectionIcon from "../components/assets/images/section-icon.png";
+import './assets/css/style.css'
 
 function Menu({ children }){
 
     return (
         <>
-        <h3> Welcome! <span>{auth.currentUser.displayName}</span>  </h3>
+        <div className="container">
+            <h3 className="section__title"> Welcome!
+                <span className="ml-2"> { auth.currentUser.displayName }</span> 
+            </h3>
+        </div>
         <div className='menu'>
             {children}
         </div>
