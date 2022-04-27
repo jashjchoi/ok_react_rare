e018dc0b5bdebee69f2144f8766a4e2fca6b77cb
blanketmanatee <ashleyl.welch@gmail.com>
2022-04-27 10:31:41 -0500

    pushing old changes to branch

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -8,7 +8,7 @@ function ContactInfo(){
                     <h1><p>Contact</p></h1>
                     <p>Connect with us on social media. Learn more about Oklahoma Rare, our founders, and events we are attending.  </p>
 
-                <p>Donate</p>
+                <h1><p>Donate</p></h1>
                 <p>Paypal button here?</p>
             </div>
         </div>
